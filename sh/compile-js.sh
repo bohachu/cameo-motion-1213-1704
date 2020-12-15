f9211871b0c7b04b1513a87443a162927c8a2115
Yueh-Cheng Chang <ycchang.pmp@gmail.com>
2020-12-15 18:06:13 +0800

    fix compile-js

--- sh/compile-js.sh
+++ sh/compile-js.sh
@@ -6,24 +6,30 @@ cd ..
 
 javascript-obfuscator cameo-divergent-stacked-bars/cameo-divergent-stacked-bars.js
 mkdir -p dist/cameo-divergent-stacked-bars
-mv cameo-divergent-stacked-bars/cameo-divergent-stacked-bars-obfuscated.js dist/cameo-divergent-stacked-bars/cameo-divergent-stacked-bars.js
+cp cameo-divergent-stacked-bars/cameo-divergent-stacked-bars-obfuscated.js dist/cameo-divergent-stacked-bars/cameo-divergent-stacked-bars.js
+rm cameo-divergent-stacked-bars/cameo-divergent-stacked-bars-obfuscated.js
 
 javascript-obfuscator cameo-run/cameo-run.js
 mkdir -p dist/cameo-run
-mv cameo-run/cameo-run-obfuscated.js dist/cameo-run/cameo-run.js
+cp cameo-run/cameo-run-obfuscated.js dist/cameo-run/cameo-run.js
+rm cameo-run/cameo-run-obfuscated.js
 
 javascript-obfuscator cameo-line/cameo-line.js
 mkdir -p dist/cameo-line
-mv cameo-line/cameo-line-obfuscated.js dist/cameo-line/cameo-line.js
+cp cameo-line/cameo-line-obfuscated.js dist/cameo-line/cameo-line.js
+rm cameo-line/cameo-line-obfuscated.js
 
 javascript-obfuscator cameo-rank/cameo-rank.js
 mkdir -p dist/cameo-rank
-mv cameo-rank/cameo-rank-obfuscated.js dist/cameo-rank/cameo-rank.js
+cp cameo-rank/cameo-rank-obfuscated.js dist/cameo-rank/cameo-rank.js
+rm cameo-rank/cameo-rank-obfuscated.js
 
 javascript-obfuscator cameo-multi-axis-prediction/cameo-multi-axis-prediction.js
 mkdir -p dist/cameo-multi-axis-prediction
-mv cameo-multi-axis-prediction/cameo-multi-axis-prediction-obfuscated.js dist/cameo-multi-axis-prediction/cameo-multi-axis-prediction.js
+cp cameo-multi-axis-prediction/cameo-multi-axis-prediction-obfuscated.js dist/cameo-multi-axis-prediction/cameo-multi-axis-prediction.js
+rm cameo-multi-axis-prediction/cameo-multi-axis-prediction-obfuscated.js
 
 javascript-obfuscator cameo-map-tw/cameo-map-tw.js
 mkdir -p dist/cameo-map-tw
-mv cameo-map-tw/cameo-map-tw-obfuscated.js dist/cameo-map-tw/cameo-map-tw.js
+cp cameo-map-tw/cameo-map-tw-obfuscated.js dist/cameo-map-tw/cameo-map-tw.js
+rm cameo-map-tw/cameo-map-tw-obfuscated.js
