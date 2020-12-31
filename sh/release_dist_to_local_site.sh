3efc24fd60f7adb9b3222bb93a378efa8d3e7986
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2020-12-31 12:06:00 +0800

    AC:echo printout

--- sh/release_dist_to_local_site.sh
+++ sh/release_dist_to_local_site.sh
@@ -8,7 +8,7 @@ source .env
 cd ..
 
 
-# overwrite homepage.html for every user
+echo "overwrite homepage.html for every user"
 if [[ -f /etc/skel/homepage.html ]]; then
     sudo rm /etc/skel/homepage.html
 fi
@@ -18,19 +18,20 @@ sudo cp homepage.html /etc/skel/
 #     sudo rm $HTML_DIR
 # fi
 
-# 複製到
+echo "複製到HTML_DIR"
 
 sudo /bin/cp -Rf dist/* $HTML_DIR
 
-# 複製到html-bak
+echo "複製到html-bak"
 if [ ! -d $HTML_DIR-bak ]; then  
     sudo mkdir -p $HTML_DIR-bak
 fi
 sudo /bin/cp -Rf dist/* $HTML_DIR-bak
 
+echo "add group analysts"
 sudo groupadd analysts
 
-# www需要讓特定使用者(如admin group)可以寫入 analysts也可寫入
+echo "www需要讓特定使用者(如admin group)可以寫入 analysts也可寫入"
 sudo chown -R root:analysts $HTML_DIR
 sudo chmod -R 775 $HTML_DIR
 sudo chown -R root:analysts $HTML_DIR-bak
@@ -48,9 +49,11 @@ sudo find . -type f -exec chmod 0774 {} \;
 
 sudo setfacl -R -m d:g:analysts:rwx $HTML_DIR
 sudo setfacl -R -m d:g:analysts:rwx $HTML_DIR-bak
-# 非群組的應該都看不到
+echo "非群組的應該都看不到"
 sudo setfacl -R -m d:o::rx $HTML_DIR
 sudo setfacl -R -m d:o::rx $HTML_DIR-bak
-# 加入權限使預設新建立的檔案都是rx權限:
+echo "加入權限使預設新建立的檔案都是rx權限:"
 sudo setfacl -R -m d:mask:r $HTML_DIR
 sudo setfacl -R -m d:mask:r $HTML_DIR-bak
+
+echo "Release dist to local site and bak completed."
