0ead6c8f2bac08bd40a3a83ca87a98fe40b5a35f
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-13 07:01:01 +0800

    AC:migrate /home/$the_user 改成0755

--- sh/migrate_current_user_hide_user_html.sh
+++ sh/migrate_current_user_hide_user_html.sh
@@ -13,8 +13,8 @@ if [[ -d $HTML_DIR/$the_user ]]; then
     echo "$HTML_DIR/$the_user 已成功移除"
 fi
 
-sudo chmod 0750 /home/$the_user
-echo "/home/$the_user 已改成0750"
+sudo chmod 0755 /home/$the_user
+echo "/home/$the_user 已改成0755"
 
 echo "處理$the_user my-web HTML目錄...完成"
 
