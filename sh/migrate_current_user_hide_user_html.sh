d8d8a8f23b0568fa54e24e05116bddf4b476b8bd
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-13 06:49:48 +0800

    AC:調整home目錄權限

--- sh/migrate_current_user_hide_user_html.sh
+++ sh/migrate_current_user_hide_user_html.sh
@@ -12,6 +12,10 @@ if [[ -d $HTML_DIR/$the_user ]]; then
     sudo rm -f $HTML_DIR/$the_user
     echo "$HTML_DIR/$the_user 已成功移除"
 fi
+
+sudo chmod 0750 /home/$the_user
+echo "/home/$the_user 已改成0750"
+
 echo "處理$the_user my-web HTML目錄...完成"
 
 
