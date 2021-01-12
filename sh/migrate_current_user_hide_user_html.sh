8ba4b782ff35d353970807c902bb3f1fc50374f1
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-13 06:27:36 +0800

    AC:fix typo

--- sh/migrate_current_user_hide_user_html.sh
+++ sh/migrate_current_user_hide_user_html.sh
@@ -4,13 +4,13 @@ the_user=$1
 source .env
 
 echo "處理$the_user my-web HTML目錄..."
-if [[ ! -d $HTML_DIR/.$new_user ]]; then
-    sudo ln -s /home/$new_user/my-web $HTML_DIR/.$new_user
-    echo "$HTML_DIR/.$new_user 已成功創建"
+if [[ ! -d $HTML_DIR/.$the_user ]]; then
+    sudo ln -s /home/$the_user/my-web $HTML_DIR/.$the_user
+    echo "$HTML_DIR/.$the_user 已成功創建"
 fi
-if [[ -d $HTML_DIR/$new_user ]]; then
-    sudo rm $HTML_DIR/$new_user
-    echo "$HTML_DIR/$new_user 已成功移除"
+if [[ -d $HTML_DIR/$the_user ]]; then
+    sudo rm -f $HTML_DIR/$the_user
+    echo "$HTML_DIR/$the_user 已成功移除"
 fi
 echo "處理$the_user my-web HTML目錄...完成"
 
