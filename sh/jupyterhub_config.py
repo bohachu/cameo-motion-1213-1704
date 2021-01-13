32afccf16a5c665d9f89b1171cdec8b9be78ad4d
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-13 10:31:06 +0800

    AC:trial add_user_cmd

--- sh/jupyterhub_config.py
+++ sh/jupyterhub_config.py
@@ -1,3 +1,4 @@
+# https://jupyterhub.readthedocs.io/en/stable/api/auth.html#pamauthenticator
 # Native Authenticator ref https://blog.jupyter.org/simpler-authentication-for-small-scale-jupyterhubs-with-nativeauthenticator-999534c77a09
 # https://native-authenticator.readthedocs.io/en/latest/index.html
 # https://github.com/jupyterhub/ldapauthenticator/issues/54
@@ -8,11 +9,16 @@ c.JupyterHub.admin_access = True
 
 # c.JupyterHub.base_rul = ''
 c.JupyterHub.bind_url = 'http://127.0.0.1:3801/'
-
-c.PAMAuthenticator.admin_groups = {'sudo'}
 c.Authenticator.admin_users = {'cameo','iek','cameo2','hanes','caro'}
 
 c.LocalAuthenticator.create_system_users = True
+
+c.PAMAuthenticator.admin_groups = {'sudo'}
+# example [‘adduser’, ‘-q’, ‘–gecos’, ‘””’, ‘–home’, ‘/customhome/USERNAME’, ‘–disabled-password’]
+# This will run the command:
+# adduser -q –gecos “” –home /customhome/river –disabled-password river
+# when the user ‘river’ is created.
+c.PAMAuthenticator.add_user_cmd = ['/root/admin_util/.add_user.sh']
 
 c.JupyterHub.ssl_key = '/var/ssl/private.key'
 c.JupyterHub.ssl_cert = '/var/ssl/certificate.crt'
