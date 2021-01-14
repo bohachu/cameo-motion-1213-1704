c6af98050b7eaf88d839b460d2a9f46caf894541
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 20:26:14 +0800

    AC: debug trial

--- sh/migrate_remove_old_ipynb.sh
+++ sh/migrate_remove_old_ipynb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #處理 ipynb更名
+
 function remove_if_exists() {
     local filepath=$1
     if [ -f $filepath ]; then
@@ -8,24 +9,36 @@ function remove_if_exists() {
         echo "sudo rm -f $filepath ...done!"
     fi
 }
-# 移除既有 my-web下 .ipynb
-List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" )
-# cd /etc/skel/my-web
-for Item in ${List[*]} 
-    do    
-        remove_if_exists /etc/skel/my-web/$Item    
-    done
 
 remove_if_exists /etc/skel/settings.ipynb
 
+
+# 移除既有 /etc/skel/my-web下 .ipynb
+List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" )
+# cd /etc/skel/my-web
+
+
+function remove_my-web_ipynb(){
+    local list=$1
+    for Item in ${list[*]} 
+    do    
+        remove_if_exists /etc/skel/my-web/$Item    
+    done
+}
+remove_my-web_ipynb $List
+
+List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
+
 function remove_html_ipynb(){
     local folderpath=$1
-    local List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
-    for Item in ${List2[*]} 
+    echo "folderpath=$folderpath"
+    local list=$2
+    for Item in ${list[*]} 
         do
             remove_if_exists $folderpath/$Item
+            echo "remove_if_exists $folderpath/$Item"
         done
 }
 
-remove_html_ipynb $HTML_DIR
-remove_html_ipynb $HTML_DIR-bak
+remove_html_ipynb $HTML_DIR $List2
+remove_html_ipynb $HTML_DIR-bak $List2
