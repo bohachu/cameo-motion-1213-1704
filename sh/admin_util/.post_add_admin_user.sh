3ba5ed6c79f99aa0a64e7ca665defdef454aa269
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 17:55:34 +0800

    AC:fix post admin邏輯

--- sh/admin_util/.post_add_admin_user.sh
+++ sh/admin_util/.post_add_admin_user.sh
@@ -19,29 +19,6 @@ source /root/admin_util/.env
 
 new_user=$1
 
-echo "處理一般使用者權限設定"
 sudo bash /root/admin_util/.post_add_user.sh $new_user
-echo "開始Admin建立後的後續處理:"
 
-echo "001 add the user to sudo and root"
-
-sudo usermod -a -G sudo $new_user
-sudo usermod -a -G root $new_user
-
-echo "002 建立admin_util連結"
-sudo ln -s /root/admin_util/.*.sh /home/$new_user/
-sudo ln -s /root/admin_util/.env /home/$new_user/
-
-echo "003 複製.ipynb"
-sudo /bin/cp /root/admin_util/*.ipynb /home/$new_user/
-
-echo "004 調整權限"
-sudo chown $new_user:$new_user /home/$new_user/*.py
-sudo chown -R $new_user:$new_user /home/$new_user/*.ipynb
-sudo chmod +x /home/$new_user/*.ipynb
-# # settings.ipynb只有admin可以操作
-sudo chmod 740 /home/$new_user/settings.ipynb
-sudo chmod 740 /home/$new_user/admin-settings.ipynb
-
-echo "Post add admin user process completed."
-
+sudo bash /root/admin_util/.post_add_admin.sh $new_user
