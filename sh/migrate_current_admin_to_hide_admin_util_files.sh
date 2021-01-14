89644050509c8253139196751c3517345cfbf99b
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 12:02:38 +0800

    AC:migrate admin不須重新刪除home *.sh,py,ipynb

--- sh/migrate_current_admin_to_hide_admin_util_files.sh
+++ sh/migrate_current_admin_to_hide_admin_util_files.sh
@@ -3,10 +3,6 @@
 the_user=$1
 source .env
 
-echo "remove $the_user obsolete admin util files"
-sudo rm /home/$the_user/*.sh
-sudo rm /home/$the_user/*.py
-sudo rm /home/$the_user/*.ipynb
 
 sudo bash migrate_current_user_hide_user_html.sh $the_user
 
