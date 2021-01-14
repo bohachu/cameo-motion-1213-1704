d742b22ca65b608264705a45c1164ddf3d1049d4
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 16:18:07 +0800

    AC:既有admin轉移,ipynb更名

--- sh/migrate_current_admin_to_hide_admin_util_files.sh
+++ sh/migrate_current_admin_to_hide_admin_util_files.sh
@@ -9,3 +9,15 @@ sudo bash migrate_current_user_hide_user_html.sh $the_user
 sudo chmod 740 /home/$the_user/*.ipynb
 
 sudo bash ./admin_util/.post_add_admin.sh $the_user
+
+#處理 ipynb更名
+function remove_if_exists() {
+    local filepath=$1
+    if [ -f $filepath ]; then
+        sudo rm -f $filepath
+        echo "sudo rm -f $filepath ...Done!"
+    fi
+}
+remove_if_exists /home/$the_user/admin-settings.ipynb
+
+
