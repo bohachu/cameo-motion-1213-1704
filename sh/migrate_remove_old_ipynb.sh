3106aa24159178e6cb789349d020de9efd9647d9
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 21:54:05 +0800

    AC: fix arry in sh

--- sh/migrate_remove_old_ipynb.sh
+++ sh/migrate_remove_old_ipynb.sh
@@ -15,11 +15,11 @@ function remove_if_exists() {
 remove_if_exists /etc/skel/settings.ipynb
 
 
-
 function remove_folder_ipynb() {
     local folderpath=$1
-    local list=$2
-    for Item in ${list[*]} 
+    shift
+    local arr=("$@")
+    for Item in "$arr[@]}";
         do  
             echo "Removing $folderpath/$Item"  
             remove_if_exists $folderpath/$Item
@@ -29,12 +29,12 @@ echo "移除既有 /etc/skel/my-web下 舊ipynb"
 List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" "fork_component.ipynb" "get_preview_address.ipynb" "get_iframe.ipynb" )
 # cd /etc/skel/my-web
 
-remove_folder_ipynb /etc/skel/my-web $List
+remove_folder_ipynb /etc/skel/my-web "${List[@]}"
 
 echo "移除 $HTML_DIR and $HTML_DIR-bak 舊ipynb"
 List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
 
-remove_folder_ipynb $HTML_DIR $List2
-remove_folder_ipynb $HTML_DIR-bak $List2
+remove_folder_ipynb $HTML_DIR "${List2[@]}"
+remove_folder_ipynb $HTML_DIR-bak "${List2[@]}"
 
 echo "移除完成"
