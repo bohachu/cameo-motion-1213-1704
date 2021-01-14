190944361f1c5c92f93ebdef83a7f40ff74ebf31
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 18:00:52 +0800

    AC:轉移admin時一併移除舊py,ipynb

--- sh/migrate_current_admin_to_hide_admin_util_files.sh
+++ sh/migrate_current_admin_to_hide_admin_util_files.sh
@@ -25,10 +25,15 @@ sudo bash migrate_current_user_hide_user_html.sh $the_user
 
 echo "remove $the_user obsolete admin util files"
 sudo rm /home/$the_user/*.sh
+sudo rm /home/$the_user/*.py
+sudo rm /home/$the_user/*.ipynb
 
 echo "Copy $the_user latest admin util files"
 sudo usermod -a -G root $the_user
 
 sudo ln -s /root/admin_util/.*.sh /home/$the_user/
 sudo ln -s /root/admin_util/*.ipynb /home/$the_user/
+
+sudo chmod 740 /home/$the_user/*.ipynb
+
 
