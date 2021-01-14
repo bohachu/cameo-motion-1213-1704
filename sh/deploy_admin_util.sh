e54adb12198f06d1a39682e729f4d455b162a867
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 10:42:10 +0800

    AC: cp .env tp /root/admin_util/

--- sh/deploy_admin_util.sh
+++ sh/deploy_admin_util.sh
@@ -12,7 +12,7 @@ sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/program/ipynb/管理者系統設�
 
 sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.*.sh /root/admin_util/
 
-sudo ln -s /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/.env /root/admin_util/.env
+sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/.env /root/admin_util/.env
 sudo chown -R root:root /root/admin_util
 sudo chmod 770 -R /root/admin_util/
 #echo "將預設admin加入root"
