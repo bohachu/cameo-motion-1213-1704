41c1460b296d0bc97f8fc082c393d970cc1169a9
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 22:04:16 +0800

    AC: try fix migrate_remove..

--- sh/migrate_remove_old_ipynb.sh
+++ sh/migrate_remove_old_ipynb.sh
@@ -17,24 +17,32 @@ remove_if_exists /etc/skel/settings.ipynb
 # ref https://askubuntu.com/questions/674333/how-to-pass-an-array-as-function-argument
 function remove_folder_ipynb() {
     local folderpath="$1"
-    shift
-    local arr=("$@")
-    for Item in "$arr[@]}";
+    local List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" "fork_component.ipynb" "get_preview_address.ipynb" "get_iframe.ipynb" )
+    for Item in ${List[*]};
         do  
             echo "Removing $folderpath/$Item"  
             remove_if_exists $folderpath/$Item
         done
 }
 echo "移除既有 /etc/skel/my-web下 舊ipynb"
-List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" "fork_component.ipynb" "get_preview_address.ipynb" "get_iframe.ipynb" )
+
 # cd /etc/skel/my-web
 
 remove_folder_ipynb /etc/skel/my-web "${List[@]}"
 
-echo "移除 $HTML_DIR and $HTML_DIR-bak 舊ipynb"
-List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
 
-remove_folder_ipynb "$HTML_DIR" "${List2[@]}"
-remove_folder_ipynb "$HTML_DIR-bak" "${List2[@]}"
+function remove_folder_ipynbㄉ() {
+    local folderpath="$1"
+    local List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
+    for Item in ${List2[*]};
+        do  
+            echo "Removing $folderpath/$Item"  
+            remove_if_exists $folderpath/$Item
+        done
+}
+echo "移除 $HTML_DIR and $HTML_DIR-bak 舊ipynb"
+
+remove_folder_ipynb2 $HTML_DIR
+remove_folder_ipynb2 $HTML_DIR-bak
 
 echo "移除完成"
