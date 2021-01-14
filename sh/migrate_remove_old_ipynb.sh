3d972a6103275ed27624ed367893263df6e71d18
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 21:45:16 +0800

    AC:migrate_remove_old_ipynb fix

--- sh/migrate_remove_old_ipynb.sh
+++ sh/migrate_remove_old_ipynb.sh
@@ -15,32 +15,26 @@ function remove_if_exists() {
 remove_if_exists /etc/skel/settings.ipynb
 
 
-# 移除既有 /etc/skel/my-web下 .ipynb
-List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" )
-# cd /etc/skel/my-web
 
-
-function remove_my-web_ipynb(){
-    local list=$1
-    for Item in ${list[*]} 
-    do    
-        remove_if_exists /etc/skel/my-web/$Item    
-    done
-}
-remove_my-web_ipynb $List
-
-List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
-
-function remove_html_ipynb(){
+function remove_folder_ipynb() {
     local folderpath=$1
-    echo "folderpath=$folderpath"
     local list=$2
     for Item in ${list[*]} 
-        do
-            remove_if_exists $folderpath/$Item
-            echo "remove_if_exists $folderpath/$Item"
-        done
+    do  
+        echo "Removing $folderpath/$Item"  
+        remove_if_exists $folderpath/$Item
+    done
 }
+echo "移除既有 /etc/skel/my-web下 舊ipynb"
+List=( "get-myweb-iframe.ipynb" "get-iframe.ipynb" "fork-component.ipynb" "get-preview-address.ipynb" "fork_component.ipynb" "get_preview_address.ipynb" "get_iframe.ipynb" )
+# cd /etc/skel/my-web
 
-remove_html_ipynb $HTML_DIR $List2
-remove_html_ipynb $HTML_DIR-bak $List2
+remove_folder_ipynb /etc/skel/my-web $List
+
+echo "移除 $HTML_DIR and $HTML_DIR-bak 舊ipynb"
+List2=( "get_iframe.ipynb" "get-www-iframe.ipynb" )
+
+remove_folder_ipynb $HTML_DIR $List2
+remove_folder_ipynb $HTML_DIR-bak $List2
+
+echo "移除完成"
