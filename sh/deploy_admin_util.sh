ca96bf35be5c32b391acc40aa83b92ae129c8beb
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 10:47:50 +0800

    AC:dpeploy admin 優化說明

--- sh/deploy_admin_util.sh
+++ sh/deploy_admin_util.sh
@@ -2,30 +2,26 @@
 # 使用情境: Deploy admin_util, 給admin使用的工具
 # 放到 /root/admin_util
 source .env
+if [[ -d /root/admin_util ]]; then
+    sudo rm -rf /root/admin_util
+    echo "已移除既有 /root/admin_util"
+fi
+
 if [[ ! -d /root/admin_util ]]; then
     sudo mkdir -p /root/admin_util
     sudo chown -R root:root /root/admin_util
     sudo chmod 770 -R /root/admin_util/
+    echo "已建立 /root/admin_util/"
 fi
 
 sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/program/ipynb/管理者系統設定.ipynb /root/admin_util/
 
 sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.*.sh /root/admin_util/
-
+echo ".*.sh copied"
 sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/.env /root/admin_util/.env
+echo ".env copied"
 sudo chown -R root:root /root/admin_util
 sudo chmod 770 -R /root/admin_util/
-#echo "將預設admin加入root"
-
-# sudo chmod +x /root/admin_util/.*.sh
-# sudo chmod +x /root/admin_util/*.ipynb
+echo "chown root:root and chmod 7780 /root/admin_util/ ...done"
 
 echo "完成佈署admin_util維護工具。"
-
-# 複製admin維護工作檔案到家目錄
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/.env /home/$new_user
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.add_user.sh /home/$new_user
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.post_add_user.sh /home/$new_user
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.add_admin_user.sh /home/$new_user
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.post_add_admin_user.sh /home/$new_user
-# sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/sh/admin_util/.setup_my-web_permission_that_user.sh /home/$new_user
