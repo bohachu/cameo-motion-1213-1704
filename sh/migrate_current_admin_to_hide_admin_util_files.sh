0dec528234abb1abbbc59e2f611b1c8e2607892e
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-14 11:50:29 +0800

    AC:fixmigrate user/admin to hide

--- sh/migrate_current_admin_to_hide_admin_util_files.sh
+++ sh/migrate_current_admin_to_hide_admin_util_files.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 the_user=$1
-# source .env
-
-
-sudo bash migrate_current_user_hide_user_html.sh $the_user
+source .env
 
 echo "remove $the_user obsolete admin util files"
 sudo rm /home/$the_user/*.sh
 sudo rm /home/$the_user/*.py
 sudo rm /home/$the_user/*.ipynb
 
-echo "Copy $the_user latest admin util files"
-sudo usermod -a -G root $the_user
-sudo ln -s /root/admin_util/.*.sh /home/$the_user/
-sudo ln -s /root/admin_util/*.ipynb /home/$the_user/
+sudo bash migrate_current_user_hide_user_html.sh $the_user
 
-sudo chmod 740 /home/$the_user/*.ipynb
+# echo "Copy $the_user latest admin util files"
+# sudo usermod -a -G root $the_user
+# sudo ln -s /root/admin_util/.*.sh /home/$the_user/
+# sudo ln -s /root/admin_util/*.ipynb /home/$the_user/
+
+# sudo chmod 740 /home/$the_user/*.ipynb
+
+sudo bash ./admin_util/.post_add_admin.sh $the_user
 
 
--- sh/migrate_current_user_hide_user_html.sh
+++ sh/migrate_current_user_hide_user_html.sh
@@ -3,6 +3,18 @@
 the_user=$1
 source .env
 
+
+sudo rm /home/$the_user/*.sh
+sudo rm /home/$the_user/*.py
+sudo rm /home/$the_user/*.ipynb
+
+sudo /bin/cp -r /etc/skel/* /home/$the_user/
+sudo chown -R $the_user:$the_user/home/$the_user/
+echo "重新複製與deploy $the_user 家目錄 完成"
+
+echo "設定my-web權限"
+sudo bash /root/admin_util/.setup_my-web_permission_that_user.sh $the_user
+
 echo "處理$the_user my-web HTML目錄..."
 if [[ ! -d $HTML_DIR/.$the_user ]]; then
     sudo ln -s /home/$the_user/my-web $HTML_DIR/.$the_user
@@ -13,12 +25,14 @@ if [[ -d $HTML_DIR/$the_user ]]; then
     echo "$HTML_DIR/$the_user 已成功移除"
 fi
 
+
 sudo chmod 0755 /home/$the_user
 echo "/home/$the_user 已改成0755"
 
-cp /etc/skel/.user-env /home/$the_user/
-echo "/home/$the_user 已取得.user-env"
+# cp /etc/skel/.user-env /home/$the_user/
+# echo "/home/$the_user 已取得.user-env"
 
 echo "處理$the_user my-web HTML目錄...完成"
+
 
 
