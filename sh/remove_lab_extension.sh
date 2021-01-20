d2b78d796dcbfe476bca172b0c57c3cf695dc732
austinyuch <yuehcheng.chang@gmail.com>
2021-01-20 22:04:02 +0800

    AC:fix sometimes error in remove lab extension

--- sh/remove_lab_extension.sh
+++ sh/remove_lab_extension.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+export NODE_OPTIONS=--max-old-space-size=4096
 sudo /opt/jupyterhub/bin/jupyter nbextension disable --py --sys-prefix ipyleaflet
 sudo /opt/jupyterhub/bin/jupyter labextension uninstall jupyter-leaflet
 sudo /opt/jupyterhub/bin/jupyter labextension uninstall keplergl-jupyter
 sudo /opt/jupyterhub/bin/jupyter labextension uninstall jupyter-matplotlib 
 sudo /opt/jupyterhub/bin/jupyter lab clean
+sudo /opt/jupyterhub/bin/jupyter lab build --minimize=False && \
+unset NODE_OPTIONS 
