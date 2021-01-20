539aee589ff93e69cb6a233abc017c41b5bf3591
austinyuch <yuehcheng.chang@gmail.com>
2021-01-20 15:06:03 +0800

    AC:fix config template

--- sh/jupyterhub_config-template.py
+++ sh/jupyterhub_config-template.py
@@ -5,7 +5,7 @@
 # To put this config in /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py, run:
  # sudo ./update_jupyterhub_config_then_restart.sh
 from jupyter_client.localinterfaces import public_ips
-import os, sys, pwd
+import os
 
 c.JupyterHub.admin_access = True
 
@@ -13,9 +13,6 @@ c.JupyterHub.admin_access = True
 # c.JupyterHub.bind_url = 'http://127.0.0.1:3801/' 會失效, jhub無法連線
 c.JupyterHub.bind_url = 'http://:3801/'
 # c.Authenticator.admin_users = {'cameo','iek','cameo2','hanes','caro'}
-
-
-If set, allowed username set is ignored.
 
 c.LocalAuthenticator.create_system_users = True
 
