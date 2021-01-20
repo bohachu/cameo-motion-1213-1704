0a525aff4809395c16f9aaa49e72d39a3e22f9cb
austinyuch <yuehcheng.chang@gmail.com>
2021-01-20 09:44:09 +0800

    AC:determine by sqlite fix

--- sh/admin_util/.determine_admin.sh
+++ sh/admin_util/.determine_admin.sh
@@ -3,12 +3,15 @@
 # ./.determine_admin.sh username sudo 
 the_user=$1
 # group_str=$2 #"1"=admin; "0"=not admin
-str_group="1"
+str_group=1
 # method 2: via sqlite and crontab
 sql_cmd="select admin from users where Name='$the_user'"
 user_group_result=$(sudo sqlite3 /jupyterhub.sqlite "$sql_cmd")
+# sample: rsa-key-20201205@iek-dev2:~$ sudo sqlite3 /jupyterhub.sqlite "select * from users where Name='cameo'"
+# 2|cameo|1|2020-12-11 04:29:30.015911|2021-01-18 16:07:19.002000|f93d966d76f047c596fa00ced0052fa8|{}|
 
-echo "sqlresult=$sqlresult"
+
+echo "user_group_result=$user_group_result"
 
 if [[ $user_group_result == *$str_group* ]]
 then
