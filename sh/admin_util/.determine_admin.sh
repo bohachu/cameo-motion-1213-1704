63175e321ad3df7479d53d2290b96b93fe49106c
austinyuch <yuehcheng.chang@gmail.com>
2021-01-20 09:40:47 +0800

    AC:determine via sqlite

--- sh/admin_util/.determine_admin.sh
+++ sh/admin_util/.determine_admin.sh
@@ -2,15 +2,15 @@
 # Determine if a user($1) is in the group($2), and return True/False
 # ./.determine_admin.sh username sudo 
 the_user=$1
-user_group_result=$(groups $the_user)
-# echo "user_group_result=$user_group_result"
+# group_str=$2 #"1"=admin; "0"=not admin
+str_group="1"
+# method 2: via sqlite and crontab
+sql_cmd="select admin from users where Name='$the_user'"
+user_group_result=$(sudo sqlite3 /jupyterhub.sqlite "$sql_cmd")
 
-group_str=$2 #"sudo"
-# echo "group_str=$group_str"
+echo "sqlresult=$sqlresult"
 
-# check if user in sudo
-# string='My long string'
-if [[ $user_group_result == *$group_str* ]]
+if [[ $user_group_result == *$str_group* ]]
 then
   #echo "this user is admin"
   echo "True"
@@ -18,3 +18,22 @@ else
   #echo "this user is not admin"
   echo "False"
 fi
+
+# # method 1 failed
+
+# user_group_result=$(groups $the_user)
+# # echo "user_group_result=$user_group_result"
+
+# group_str=$2 #"sudo"
+# # echo "group_str=$group_str"
+
+# # check if user in sudo
+# # string='My long string'
+# if [[ $user_group_result == *$group_str* ]]
+# then
+#   #echo "this user is admin"
+#   echo "True"
+# else
+#   #echo "this user is not admin"
+#   echo "False"
+# fi
