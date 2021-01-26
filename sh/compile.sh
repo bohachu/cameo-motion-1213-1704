0eafb9b773c6f8887fd10298214fb608a320acee
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-26 10:42:13 +0800

    AC:compile for lib v2

--- sh/compile.sh
+++ sh/compile.sh
@@ -6,7 +6,7 @@
 source .env
 
 
-lib_version=$JS_LIB_VERSION
+lib_version=$JS_LIB_VERSION #v2
 
 # 回到專案目錄
 cd /home/$INSTALL_USER/$PRJ_DIR_NAME
@@ -19,12 +19,21 @@ fi
 if [[ ! -d /home/$INSTALL_USER/$PRJ_DIR_NAME/dist ]]; then
     mkdir /home/$INSTALL_USER/$PRJ_DIR_NAME/dist
 fi
-if [[ ! -d /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version ]]; then
-    mkdir -p /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version
+if [[ ! -d /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version ]]; then
+    mkdir -p /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version
+fi
+if [[ ! -d /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cdn.amcharts.com/lib/4 ]]; then
+    mkdir -p /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cdn.amcharts.com/lib/4
+fi
+if [[ ! -d /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/www.amcharts.com/lib/4/plugins ]]; then
+    mkdir -p /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/www.amcharts.com/lib/4/plugins
 fi
 
-/bin/cp -R /home/$INSTALL_USER/$PRJ_DIR_NAME/lib/ /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
+/bin/cp -R /home/$INSTALL_USER/$PRJ_DIR_NAME/cameo-motion/lib/$lib_version /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version
 /bin/cp -R /home/$INSTALL_USER/$PRJ_DIR_NAME/app-* /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
+/bin/cp -R /home/$INSTALL_USER/$PRJ_DIR_NAME/www.amcharts.com/lib/4/plugins/* /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/www.amcharts.com/lib/4/plugins/
+/bin/cp -R /home/$INSTALL_USER/$PRJ_DIR_NAME/cdn.amcharts.com/lib/4/* /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cdn.amcharts.com/lib/4/
+
 # cp -R cameo-* dist/
 echo "Copied app-* and cameo-* modules to dist."
 /bin/cp -R img /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
@@ -33,6 +42,7 @@ echo "Copied img and scss folder."
 /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/iek-carousel.html /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
 /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/index.html /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
 /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/favicon.ico /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
+/bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/homepage.html /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/
 echo "Copied favicon.ico and *.html."
 
 function compile_module() {
@@ -46,19 +56,19 @@ function compile_module() {
     echo "Compiled js: $module_path/$js_name.js ."
 }
 # echo "compile_module dist/lib/$lib_version/cameo-core cameo-df"
-compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/cameo-core" "cameo-df"
-# echo "compile_module /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/cameo-core cameo-load"
-compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/cameo-core" "cameo-load"
+# compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version/cameo-core" "cameo-df"
+# echo "compile_module /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version/cameo-core cameo-load"
+# compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version" "cameo-load"
 # echo "compile_module /home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/cameo-motion/cameo-table cameo-table" 
-compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/cameo-motion/cameo-table" "cameo-table"
+# compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version/cameo-motion/cameo-table" "cameo-table"
 
 
-List=( "cameo-divergent-stacked-bars" "cameo-line" "cameo-map-tw" "cameo-multi-axis-prediction" "cameo-rank" "cameo-run" )
+List=( "cameo-am-element" "cameo-clean" "cameo-divergent-stacked-bars" "cameo-line" "cameo-map-tw" "cameo-multi-axis-prediction" "cameo-rank" "cameo-run" )
 
 for Item in ${List[*]} 
   do
     # echo "compile_module dist/lib/$lib_version/$Item $Item"
-    compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/lib/$lib_version/$Item" $Item
+    compile_module "/home/$INSTALL_USER/$PRJ_DIR_NAME/dist/cameo-motion/lib/$lib_version/$Item" $Item
   done
 
 # TODO: 未來需要修改路徑, 讓dist 和www 目錄不須有components (須同步修改release_dist_to_my-web.sh)
