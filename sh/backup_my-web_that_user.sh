9b6c09887c84401602b87249ac17d587f96dda99
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-26 15:05:19 +0800

    AC:chown backuped my-web

--- sh/backup_my-web_that_user.sh
+++ sh/backup_my-web_that_user.sh
@@ -5,5 +5,6 @@ the_user=$1
 TODAY=$(date +"%Y%m%d")
 
 sudo /bin/cp -r /home/$the_user/my-web/ /home/$the_user/history/my-web$TODAY/
+sudo chown $the_user:$the_user -R /home/$the_user/history/my-web$TODAY
 
 echo "$the_user/my-web/ was backuped to $the_user/history/my-web$TODAY/"
