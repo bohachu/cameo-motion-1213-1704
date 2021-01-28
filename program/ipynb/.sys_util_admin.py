ef0e6eb950af9bbec71f2a91326777c9f172e3b8
Yueh-Cheng Chang <yuehcheng.chang@gmail.com>
2021-01-28 23:29:44 +0800

    AC:checkpoint refactor ipynb

---
+++ program/ipynb/.sys_util_admin.py
@@ -0,0 +1,138 @@
+import os, shutil
+from distutils import dir_util
+import getpass
+
+def get_user_home_path():
+    username=getpass.getuser()
+    str_path_home = f'/home/{username}'
+    return str_path_home
+
+str_user_home_path=get_user_home_path()
+
+def www_copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        print("item = "+item)
+        if item == "my-web": # first item will be my-web which is folder name, but we don't want to create a folder name
+            continue
+        if item == "www功能設定.ipynb":
+            continue
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.islink(s): # dont copy if it is a symbolic link
+            continue
+        if os.path.isdir(s):
+            #shutil.copytree(s, d, symlinks,  ignore=shutil.ignore_patterns('.*')) #my-web copy using this
+            dir_util.copy_tree(s,d) # www copy using this
+        else:
+            #shutil.copy2(s, d)
+            shutil.copyfile(s, d)
+
+def backup_www(output_www_reset):
+    with output_www_reset:
+        print("www 備份中...")
+    # str_user_home_path=get_user_home_path()
+    src_folder=os.path.join(str_user_home_path,"www")
+    # original_folder_name="./history/www"
+    original_folder_name = os.path.join(str_user_home_path,"history","www")
+    new_folder_name=os.path.join(str_user_home_path,"history","www")
+    folder_duplicate_number=0
+    print("test")
+    while 1:
+        if not os.path.exists(new_folder_name):
+            print("if")
+            print(new_folder_name)
+            os.makedirs(new_folder_name)
+            break
+        else:
+
+            folder_duplicate_number+=1
+            new_folder_name=original_folder_name+"-"+str(folder_duplicate_number)
+            print("else")
+            print(new_folder_name)
+    www_copytree(src_folder,new_folder_name)
+       
+    with output_www_reset:
+        print("www 備份完成")
+
+    
+def reset_www(output_myweb_reset):
+    with output_myweb_reset:
+        print("www 重置中...")
+    src_folder="/var/www/iek.cameo.tw/html-bak"
+    # str_user_home_path = get_user_home_path()
+    new_folder_name=os.path.join(str_user_home_path,"history","www")
+    # new_folder_name="./www"
+
+#     if os.path.exists(new_folder_name):
+#        shutil.rmtree(new_folder_name)
+    www_copytree(src_folder,new_folder_name)
+    with output_myweb_reset:
+        print("www 重置完成")
+
+def on_button_clicked_www_reset(output_myweb_reset):
+    backup_www(output_myweb_reset)
+    reset_www(output_myweb_reset)
+
+def myweb_copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        print("item = "+item)
+        if item == "my-web": # first item will be my-web which is folder name, but we don't want to create a folder name
+            continue
+        if item == "www功能設定.ipynb":
+            continue
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.islink(s): # dont copy if it is a symbolic link
+            continue
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks,  ignore=shutil.ignore_patterns('.*')) #my-web copy using this
+            #dir_util.copy_tree(s,d) # www copy using this
+        else:
+            shutil.copy2(s, d)
+            #shutil.copyfile(s, d)
+
+
+def reset_my_web(output_myweb_reset):
+    with output_myweb_reset:
+        print("my-web 重置中...")
+    src_folder="/etc/skel/my-web"
+
+    # str_user_home_path = get_user_home_path()
+    # new_folder_name="./my-web"
+    new_folder_name=os.path.join(str_user_home_path,"history","my-web")
+
+    if os.path.exists(new_folder_name):
+        shutil.rmtree(new_folder_name)
+    if not os.path.exists(new_folder_name):
+        os.mkdir(new_folder_name)
+    myweb_copytree(src_folder,new_folder_name)
+    with output_myweb_reset:
+        print("my-web 重置完成")
+
+
+def backup_my_web(output_myweb_reset):
+    with output_myweb_reset:
+        print("my-web 備份中...")
+    str_user_home_path = get_user_home_path()
+    src_folder = os.path.join(str_user_home_path,"my-web")
+    # src_folder="./my-web"
+    # original_folder_name="./history/my-web"
+    original_folder_name = os.path.join(str_user_home_path,"history","my-web")
+    # new_folder_name="./history/my-web"
+    new_folder_name = os.path.join(str_user_home_path,"history","my-web")
+    folder_duplicate_number=0
+    while 1:
+        if not os.path.exists(new_folder_name):
+            os.makedirs(new_folder_name)
+            break
+        else:
+            folder_duplicate_number+=1
+            new_folder_name=original_folder_name+"-"+str(folder_duplicate_number)
+    myweb_copytree(src_folder,new_folder_name)
+       
+    with output_myweb_reset:
+        print("my-web 備份至"+new_folder_name)
+
+def on_button_clicked_myweb_reset(output_myweb_reset):
+    backup_my_web(output_myweb_reset)
+    reset_my_web(output_myweb_reset)
--- sh/release_user_util.sh
+++ sh/release_user_util.sh
@@ -14,9 +14,10 @@ if [[ ! -d /root/user_util ]]; then
     echo "已建立 /root/admin_util/"
 fi
 
+sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/program/ipynb/.sys_util_user.py /root/user_util/
+echo ".sys_util_user.py copied"
 sudo /bin/cp /home/$INSTALL_USER/$PRJ_DIR_NAME/program/ipynb/get-iframe.ipynb /root/user_util/
 echo "get-iframe.ipynb copied"
-
 sudo chown -R root:analysts /root/user_util
 sudo chmod 755 -R /root/user_util/
 echo "sudo chown -R root:analysts /root/user_util ...done"
